2bd9a8a544ab7737695c09399c51954617e26afd
Xavier Flores <xa_franc@live.com>
2024-03-14 13:57:33 -0600

    initial config

---
+++ api/src/index.ts
@@ -0,0 +1,18 @@
+import {ApolloServer} from '@apollo/server';
+
+const typeDefs = `
+type Query {
+  info: String!
+}
+`
+const resolvers = {
+  Query:{
+    info: ()=>`This is the API of Platzi Node GraphQL`
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
