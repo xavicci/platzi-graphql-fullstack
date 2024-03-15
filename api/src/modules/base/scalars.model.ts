200d0becf7e33bba6e8ddf41aef7fc7e1867457b
Xavier Flores <xa_franc@live.com>
2024-03-15 13:53:20 -0600

    finish config prima shema and sqlite

---
+++ api/src/modules/base/scalars.model.ts
@@ -0,0 +1,20 @@
+import { GraphQLScalarType, Kind } from 'graphql'
+
+export const DateTime = new GraphQLScalarType({
+  name: 'DateTime',
+  description: 'Represents a date time object',
+  serialize(value) {
+    // @ts-ignore
+    return value.toISOString() // Convert outgoing Date to ISOString for JSON
+  },
+  parseValue(value) {
+    // @ts-ignore
+    return new Date(value) // Convert incoming integer to Date
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)) // Convert hard-coded AST string to integer and then to Date
+    }
+    return null // Invalid hard-coded value (not an integer)
+  },
+})
